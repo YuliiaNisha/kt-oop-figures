c46796dce14a50a95606d3c792dee86ed091f415
Yuliia <innishastudy@gmail.com>
2026-03-04 20:11:31 +0200

    removed magic numbers"

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,6 +3,9 @@ package mate.academy
 import mate.academy.model.Figure
 import mate.academy.service.FigureSupplier
 
+const val FIGURES_NUMBER = 6
+const val TWO = 2
+
 fun main() {
     val figures = getFigures()
 
@@ -13,9 +16,9 @@ fun main() {
 
 fun getFigures() : Array<Figure> {
     val supplier = FigureSupplier()
-    val size = 6
+    val size = FIGURES_NUMBER
     return Array(size) { index ->
-        if (index < size / 2) {
+        if (index < size / TWO) {
             supplier.getRandomFigure()
         } else {
             supplier.getDefaultFigure()
--- src/main/kotlin/mate/academy/service/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/service/FigureSupplier.kt
@@ -1,16 +1,31 @@
 package mate.academy.service
 
-import mate.academy.model.*
+import mate.academy.model.Square
+import mate.academy.model.RightTriangle
+import mate.academy.model.Rectangle
+import mate.academy.model.IsoscelesTrapezoid
+import mate.academy.model.Circle
+import mate.academy.model.Figure
+import mate.academy.model.Color
+
+const val MAX_NUMBER_OF_FIGURES = 5
+const val DEFAULT_RADIUS = 10.0
+const val MIN_VALUE = 1
+const val MAX_VALUE = 50
+const val ONE = 1
+const val TWO = 2
+const val THREE = 3
+const val FOUR = 4
 
 class FigureSupplier {
     private val supplier = ColorSupplier()
 
     fun getRandomFigure(): Figure {
-        return when ((1..5).random()) {
-            1 -> Square(supplier.getRandomColor(), getRandomDouble())
-            2 -> Rectangle(supplier.getRandomColor(), getRandomDouble(), getRandomDouble())
-            3 -> RightTriangle(supplier.getRandomColor(), getRandomDouble(), getRandomDouble())
-            4 -> Circle(supplier.getRandomColor(), getRandomDouble())
+        return when ((MIN_VALUE..MAX_NUMBER_OF_FIGURES).random()) {
+            ONE -> Square(supplier.getRandomColor(), getRandomDouble())
+            TWO -> Rectangle(supplier.getRandomColor(), getRandomDouble(), getRandomDouble())
+            THREE -> RightTriangle(supplier.getRandomColor(), getRandomDouble(), getRandomDouble())
+            FOUR -> Circle(supplier.getRandomColor(), getRandomDouble())
             else -> IsoscelesTrapezoid(
                 supplier.getRandomColor(),
                 getRandomDouble(), getRandomDouble(),
@@ -19,10 +34,10 @@ class FigureSupplier {
     }
 
     fun getDefaultFigure(): Figure {
-        return Circle(Color.WHITE, 10.0)
+        return Circle(Color.WHITE, DEFAULT_RADIUS)
     }
 
     private fun getRandomDouble() : Double {
-        return (1..50).random().toDouble()
+        return (MIN_VALUE..MAX_VALUE).random().toDouble()
     }
 }
